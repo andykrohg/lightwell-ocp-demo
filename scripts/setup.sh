457fe15f4fb4911d5b5cded303db2f3d60601fd4
Andy Krohg <akrohg@redhat.com>
2026-09-10 19:51:56 -0400

    Fix rpa issuer URL

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -70,7 +70,7 @@ sed \
   -e "s|__DEMO_NAMESPACE__|${DEMO_NAMESPACE}|g" \
   -e "s|__REGISTRY_HOST__|${REGISTRY_HOST}|g" \
   -e "s|__TPA_URL__|${TPA_URL}|g" \
-  -e "s|__TPA_OIDC_ISSUER_URL__|${TPA_OIDC_ISSUER}|g" \
+  -e "s|__TPA_OIDC_ISSUER_URL__|${TPA_OIDC_ISSUER_URL}|g" \
   -e "s|__TPA_CLIENT_SECRET__|${TPA_CLIENT_SECRET}|g" \
   -e "s|__ROX_CENTRAL_ENDPOINT__|${ROX_CENTRAL_ENDPOINT}|g" \
   -e "s|__ROX_API_TOKEN__|${ROX_API_TOKEN}|g" \
