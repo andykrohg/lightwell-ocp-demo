fa1e0e65cf60f8c2f04905bc09cc7648d8dc3f18
Andy Krohg <akrohg@redhat.com>
2026-09-10 18:06:54 -0400

    Point demo hub pipeline links to CI namespace

--- demo-hub/entrypoint.sh
+++ demo-hub/entrypoint.sh
@@ -6,8 +6,9 @@ export ACS_CONSOLE_URL="${ACS_CONSOLE_URL:-https://central-acs.apps.example.com}
 export OCP_CONSOLE_URL="${OCP_CONSOLE_URL:-https://console-openshift-console.apps.example.com}"
 export DEMO_NAMESPACE="${DEMO_NAMESPACE:-lightwell-demo}"
 export APPS_DOMAIN="${APPS_DOMAIN:-apps.example.com}"
+export CI_NAMESPACE="${CI_NAMESPACE:-${DEMO_NAMESPACE}-ci}"
 
-envsubst '${TPA_CONSOLE_URL} ${ACS_CONSOLE_URL} ${OCP_CONSOLE_URL} ${DEMO_NAMESPACE} ${APPS_DOMAIN}' \
+envsubst '${TPA_CONSOLE_URL} ${ACS_CONSOLE_URL} ${OCP_CONSOLE_URL} ${DEMO_NAMESPACE} ${APPS_DOMAIN} ${CI_NAMESPACE}' \
   < /opt/app-root/src/index.template.html \
   > /opt/app-root/src/index.html
 
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -148,6 +148,7 @@ kustomize build "$PROJECT_DIR/manifests/overlays/dashboard" \
     -e "s|__OCP_CONSOLE_URL__|${OCP_CONSOLE_URL}|g" \
     -e "s|__DEMO_NAMESPACE__|${DEMO_NAMESPACE}|g" \
     -e "s|__APPS_DOMAIN__|${APPS_DOMAIN}|g" \
+    -e "s|__CI_NAMESPACE__|${CI_NAMESPACE}|g" \
   | oc apply -n "$DEMO_NAMESPACE" -f -
 
 banner "Setup Complete"
